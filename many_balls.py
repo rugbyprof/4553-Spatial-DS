c0b3fc8c0459ff23a536af1f17ee9c503193eda4
Terry Griffin <griffin@Terrys-Mac-mini.local>
2015-09-08 15:17:44 -0500

    adding code discussed in class

--- many_balls.py
+++ many_balls.py
@@ -4,17 +4,17 @@ import csv
 import random
 
 class ManyBalls(pantograph.PantographHandler):
-    
+
     def setup(self):
         self.numBalls = 500
 
         self.balls = []
         self.createBalls()
-        
+
     def createBalls(self):
         for i in range(self.numBalls):
             self.balls.append([random.uniform(1, self.width),random.uniform(1,self.height),"#%06x" % random.randint(0, 0xFFFFFF),random.uniform(3,15)])
-    
+
     def moveBalls(self):
         for i in range(self.numBalls):
             negate = random.randint(0,311) % 2
@@ -26,28 +26,26 @@ class ManyBalls(pantograph.PantographHandler):
             d2 = random.randint(1, 1) * random.randint(1, 1) * negate
             self.balls[i][0] += d1
             self.balls[i][1] += d2
-            
+
     def drawBalls(self):
          for i in range(self.numBalls):
-            self.fill_circle(self.balls[i][0], self.balls[i][1], self.balls[i][3], color = self.balls[i][2])   
-            
+            self.fill_circle(self.balls[i][0], self.balls[i][1], self.balls[i][3], color = self.balls[i][2])
+
     def resizeBalls(self):
         for i in range(self.numBalls):
             if random.randint(0, 311) % 2 == 0:
                 negate = -1
             else:
                 negate = 1
-                
+
             self.balls[i][3] = self.balls[i][3] + (random.randint(0,5) * negate)
 
     def update(self):
         self.moveBalls()
         self.clear_rect(0, 0, self.width, self.height)
-        self.resizeBalls()
+        #self.resizeBalls()
         self.drawBalls()
 
-
-        
 if __name__ == '__main__':
     app = pantograph.SimplePantographApplication(ManyBalls)
     app.run()
