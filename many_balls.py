ec67880475f90ccacc4f96e9879f8040dbdb0eda
rugbyprof <terry.griffin@mwsu.edu>
2015-09-08 14:00:15 -0500

    added many balls example

--- many_balls.py
+++ many_balls.py
@@ -3,18 +3,17 @@ import math
 import csv
 import random
 
-class MyStuff(pantograph.PantographHandler):
+class ManyBalls(pantograph.PantographHandler):
     
     def setup(self):
         self.numBalls = 500
 
         self.balls = []
         self.createBalls()
-        print(self.balls)
         
     def createBalls(self):
         for i in range(self.numBalls):
-            self.balls.append([random.uniform(1, self.width),random.uniform(1,self.height),"#%06x" % random.randint(0, 0xFFFFFF)])
+            self.balls.append([random.uniform(1, self.width),random.uniform(1,self.height),"#%06x" % random.randint(0, 0xFFFFFF),random.uniform(3,15)])
     
     def moveBalls(self):
         for i in range(self.numBalls):
@@ -30,24 +29,25 @@ class MyStuff(pantograph.PantographHandler):
             
     def drawBalls(self):
          for i in range(self.numBalls):
-            self.fill_circle(self.balls[i][0], self.balls[i][1], 10, color = self.balls[i][2])       
+            self.fill_circle(self.balls[i][0], self.balls[i][1], self.balls[i][3], color = self.balls[i][2])   
+            
+    def resizeBalls(self):
+        for i in range(self.numBalls):
+            if random.randint(0, 311) % 2 == 0:
+                negate = -1
+            else:
+                negate = 1
+                
+            self.balls[i][3] = self.balls[i][3] + (random.randint(0,5) * negate)
 
     def update(self):
         self.moveBalls()
         self.clear_rect(0, 0, self.width, self.height)
+        self.resizeBalls()
         self.drawBalls()
-        
-            
-    def loadCities():
-        self.citys = []
-        with open('citylist.csv', 'rb') as csvfile:
-            citysCsv = csv.reader(csvfile, delimiter=',', quotechar='"')
-            for city in citysCsv:
-                self.citys.append({"Name":city[0],"Country":city[1],"lat":city[2],"lon":city[3]})
-            #print(self.citys
 
 
         
 if __name__ == '__main__':
-    app = pantograph.SimplePantographApplication(MyStuff)
+    app = pantograph.SimplePantographApplication(ManyBalls)
     app.run()
