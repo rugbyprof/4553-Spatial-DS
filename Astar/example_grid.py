3320046fdc6d1b3a62f0ba5ab411c22818299455
rugbyprof <terry.griffin@mwsu.edui>
2015-12-03 12:43:28 -0600

    got astar working with pantograph
    Former-commit-id: 5c73a81267e66fe9da9cb8b437bef79676f79863 [formerly 22d5121c9d910d8dcc97f185a7b8214cdc753227]
    Former-commit-id: 3e27e4823f42b0a1208b55222e166195f6de36fe

--- Astar/example_grid.py
+++ Astar/example_grid.py
@@ -1,12 +1,79 @@
-#! /usr/bin/env python
-
-#https://gist.github.com/benaxelrod/57dd9202a93e8295c2c1#file-astar-py
-
-from astar_grid import AStarGrid, AStarGridNode
+from math import sqrt
 from itertools import product
+import pantograph
+
+
+class AStar():
+    def __init__(self, graph):
+        self.graph = graph
+        self.moves = []
+
+    def heuristic(self, node, start, end):
+        raise NotImplementedError
+
+    def search(self, start, end):
+        openset = set()
+        closedset = set()
+        current = start
+        openset.add(current)  #lets add the start coordinate
+        while openset:  #openset still has items in it
+            current = min(openset, key=lambda o:o.g + o.h)  #
+            if current == end:
+                path = []
+                while current.parent:
+                    path.append(current)
+                    current = current.parent
+                path.append(current)
+                return path[::-1]   #reverse the path
+            openset.remove(current)
+            closedset.add(current)
+            for node in self.graph[current]:
+                if node in closedset:
+                    continue
+                if node in openset:
+                    new_g = current.g + current.move_cost(node)
+                    if node.g > new_g:
+                        node.g = new_g
+                        node.parent = current
+                else:
+                    node.g = current.g + current.move_cost(node)
+                    node.h = self.heuristic(node, start, end)
+                    node.parent = current
+                    openset.add(node)
+        return None
+
+class AStarNode(object):
+    def __init__(self):
+        self.g = 0
+        self.h = 0
+        self.parent = None
+
+    def move_cost(self, other):
+        raise NotImplementedError
+
+class AStarGrid(AStar):
+    def heuristic(self, node, start, end):
+        # NOTE: this is traditionally sqrt((end.x - node.x)**2 + (end.y - node.y)**2)
+        # However, if you are not interested in the *actual* cost, but only relative cost,
+        # then the math can be simplified.
+        return abs(end.x - node.x) + abs(end.y - node.y)
+        #return sqrt((end.x - node.x)**2 + (end.y - node.y)**2)
+
+class AStarGridNode(AStarNode):
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        super(AStarGridNode, self).__init__()
+
+    def move_cost(self, other):
+        diagonal = abs(self.x - other.x) == 1 and abs(self.y - other.y) == 1
+        return 14 if diagonal else 10
+
+    def __repr__(self):
+        return '(%d %d)' % (self.x, self.y)
 
 def make_graph(mapinfo):
     nodes = [[AStarGridNode(x, y) for y in range(mapinfo['height'])] for x in range(mapinfo['width'])]
+
     graph = {}
     for x, y in product(range(mapinfo['width']), range(mapinfo['height'])):
         node = nodes[x][y]
@@ -18,11 +85,160 @@ def make_graph(mapinfo):
             graph[nodes[x][y]].append(nodes[x+i][y+j])
     return graph, nodes
 
-graph, nodes = make_graph({"width": 8, "height": 8, "obstacle": [[2,5],[3,5],[4,5],[5,5]]})
-paths = AStarGrid(graph)
-start, end = nodes[1][1], nodes[5][7]
-path = paths.search(start, end)
-if path is None:
-    print "No path found"
-else:
-    print "Path found:", path
+
+
+class DrawAstar(pantograph.PantographHandler):
+
+    def setup(self):
+
+        self.block = 10
+        self.obstacles = []
+        self.adjObstacles = []
+        self.startCoord = None
+        self.finishCoord = None
+        self.pathFound = False
+        self.path = None
+
+        print self.width/self.block
+        print self.height/self.block
+
+    def startAstar(self):
+        graph, nodes = make_graph({"width": self.width/self.block, "height": self.height/self.block, "obstacle": self.adjObstacles})
+
+        paths = AStarGrid(graph)
+        startx,starty = self.startCoord
+        finishx,finishy = self.finishCoord
+
+        startx /= self.block
+        starty /= self.block
+        finishx /= self.block
+        finishy /= self.block
+
+        start, end = nodes[startx][starty], nodes[finishx][finishy]
+        path = paths.search(start, end)
+        if path is None:
+            print "No path found"
+        else:
+            print "Path found:", path
+            self.path = path
+            self.pathFound = True
+
+
+    def update(self):
+        self.clear_rect(0, 0, self.width, self.height)
+        self.drawGrid()
+        self.drawObstacles()
+        self.drawStartFinish()
+        if self.pathFound:
+            self.drawPath()
+
+    def drawPath(self):
+        for p in self.path:
+            x,y = self.adjustCoords(p.x*self.block,p.y*self.block)
+            self.fill_rect(x, y, self.block , self.block , "#00F")
+
+    def refreshWorld(self):
+        self.pathFound = False
+        self.path = []
+
+    def drawGrid(self):
+        for i in range(0,self.width,self.block):
+           self.draw_line(i, 0, i, self.height, "#AAAAAA")
+           self.draw_line(0, i, self.width, i , "#AAAAAA")
+
+    # def toggleObstacle(self,x,y):
+    #     for i in range(-self.block,self.block,self.block):
+    #         for j in range(-self.block,self.block,self.block):
+    #             gridX = x+i
+    #             gridY = y+j
+    #             adjX = gridX / self.block
+    #             adjY = gridY / self.block
+    #             if [gridX,gridY] not in self.obstacles:
+    #                 self.obstacles.append([gridX,gridY])
+    #                 self.adjObstacles.append([adjX,adjY])
+    #             else:
+    #                 self.obstacles.remove([gridX,gridY])
+    #                 self.adjObstacles.remove([adjX,adjY])
+    #             #print self.obstacles
+    #             #print self.adjObstacles
+
+    def toggleObstacle(self,x,y):
+        gridX = x
+        gridY = y
+        adjX = gridX / self.block
+        adjY = gridY / self.block
+        if [gridX,gridY] not in self.obstacles:
+            self.obstacles.append([gridX,gridY])
+            self.adjObstacles.append([adjX,adjY])
+        else:
+            self.obstacles.remove([gridX,gridY])
+            self.adjObstacles.remove([adjX,adjY])
+        #print self.obstacles
+        #print self.adjObstacles
+
+    def drawObstacles(self):
+        for r in self.obstacles:
+            self.fill_rect(r[0], r[1], self.block , self.block , "#000")
+
+    def drawStartFinish(self):
+        if self.startCoord:
+            x,y = self.startCoord
+            self.fill_rect(x, y, self.block , self.block , "#0F0")
+
+        if self.finishCoord:
+            x,y = self.finishCoord
+            self.fill_rect(x, y, self.block , self.block , "#F00")
+
+    def on_mouse_down(self,e):
+        print e
+        #Alt key gets you bigger blocks
+        x,y = self.adjustCoords(e.x,e.y)
+
+        if not e.alt_key and not e.ctrl_key and not e.meta_key:
+            self.toggleObstacle(x,y)
+
+        if e.alt_key:
+            pass
+        elif e.ctrl_key:
+            self.addStart(x,y)
+        elif e.meta_key:
+            self.addFinish(x,y)
+        elif e.shift_key:
+            self.drawLine(x,y)
+
+    def drawLine(self,x,y):
+        for i in range(0,self.width,self.block):
+            print i,y
+            self.toggleObstacle(i,y)
+
+    def on_dbl_click(self,e):
+        print e
+        self.startAstar()
+
+    def on_key_down(self,e):
+        print e
+        if e.key_code==32: # S key
+            self.refreshWorld()
+        if e.key_code==83: # S key
+            self.addStart(x,y)
+        elif e.key_code==70: # F key
+            self.addFinish(x,y)
+
+    def addStart(self,x,y):
+        self.fill_rect(x, y, self.block , self.block , "#F00")
+        self.startCoord =(x,y)
+
+    def addFinish(self,x,y):
+        self.fill_rect(x, y, self.block , self.block , "#0F0")
+        self.finishCoord = (x,y)
+
+    def adjustCoords(self,x,y):
+        """adjust the coords to fit our grid"""
+        x = (x / self.block) * self.block
+        y = (y / self.block) * self.block
+        return (x,y)
+
+
+if __name__ == '__main__':
+    app = pantograph.SimplePantographApplication(DrawAstar)
+    app.run()
