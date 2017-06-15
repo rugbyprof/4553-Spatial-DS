deff3f1517c0d7a4a04a0d9a5edcccb9975f62ba
rugbyprof <terry.griffin@mwsu.edui>
2017-06-15 14:22:49 -0500

    added some resources

--- Resources/Dbscan_Ex/main.py
+++ Resources/Dbscan_Ex/main.py
@@ -38,14 +38,6 @@ pygame.display.flip()
 epsilon = 20
 min_pts = 5.0
 
-# points = [(3,16),(3,14),(4,15),(5,14),(5,17),(6,17),(6,16),(6,15),(6,13),
-#             (10,6),(10,4),(10,3),(11,4),(12,5),(12,4),(12,3),
-#             (18,13),(19,14),(19,13),(19,2),(20,14),(20,13),(20,12),(20,11),(21,13),
-#             (23,6),(23,5),(23,4),(23,3),(23,2),(24,5),(24,4),(24,3),(24,2),
-#             (10,11),(14,11),(13,16),(19,5),(25,13)]
-
-#points = [(0,0),(random.randrange(0,width),random.randrange(0,height)),(width,height)]
-
 points = []
 
 for i in range(500):
@@ -57,7 +49,6 @@ mbrs = calculate_mbrs(points, epsilon, min_pts)
 
 running = True
 while running:
-    #pygame.draw.lines(screen, black, False, points, 2)
 
     for p in points:
         pygame.draw.circle(screen, black, p, 3, 0)
