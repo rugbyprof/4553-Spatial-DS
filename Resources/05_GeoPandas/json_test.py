7f48f1a8fed5efa59394b96b61441296f214d451
rugbyprof <terry.griffin@msutexas.edu>
2022-01-16 22:37:47 -0600

    Pushed from Chickenbutt-15

---
+++ Resources/05_GeoPandas/json_test.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from rich import print
+
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    }
+)
+
+print(df)
+
+
+print(df["Age"])
